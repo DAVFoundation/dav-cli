2d414a1449bcf0731b35014bb7c25cbcb3ce5063
Tal Ater <tal@talater.com>
2017-12-23 00:14:36 +0200

    Run a ganache-cli server when running the cli

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const version = require("./lib/version");
 console.log(`DAV CLI v${version} - makes developing with DAV easy`);
 console.log();
 
+const ganache = require("ganache-cli");
+const server = ganache.server();
+const port = 8545;
+server.listen(port, (err, blockchain) => {
+  console.log(`Local Ethereum node running. Port ${port}.`);
+});
