ab2acd724638139062960dd54d111e02f3923a49
Tal Ater <tal@talater.com>
2017-12-23 01:04:19 +0200

    Tweaked CLI description

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const ganache = require('ganache-cli');
 // Configure the CLI
 program
   .version(version)
-  .description('makes developing with DAV easy')
   .option('--start', 'Start a local Ethereum node')
+  .description(`DAV CLI v${version} - makes developing with DAV easy`)
   .option('-p, --port <n>', 'Port for Ethereum node to listen to')
   .parse(process.argv);
 
