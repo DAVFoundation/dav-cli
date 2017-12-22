302ef59bca4de696c7b001cb86412e7f9afb9153
Tal Ater <tal@talater.com>
2017-12-23 00:44:30 +0200

    Fixed linting errors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const version = require("./lib/version");
-const OS = require("os");
+const version = require('./lib/version');
+const OS = require('os');
 
 console.log(`DAV CLI v${version} - makes developing with DAV easy`+OS.EOL);
 
-const ganache = require("ganache-cli");
+const ganache = require('ganache-cli');
 const server = ganache.server();
 const port = 8545;
-server.listen(port, (err, blockchain) => {
+server.listen(port, (/*err, blockchain*/) => {
   console.log(`Local Ethereum node running. Listening on port ${port}.`);
 });
