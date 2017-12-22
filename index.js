74d7ff7f1e1fee557630a369a638af2a401b088e
Tal Ater <tal@talater.com>
2017-12-23 01:00:31 +0200

    Added commander to main cli files

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const program = require('commander');
 const version = require('./lib/version');
 const OS = require('os');
 const ganache = require('ganache-cli');
 
+// Configure the CLI
+program
+  .version(version)
+  .description('makes developing with DAV easy')
+  .option('--start', 'Start a local Ethereum node')
+  .parse(process.argv);
+
 console.log(`DAV CLI v${version} - makes developing with DAV easy`+OS.EOL);
 
-const server = ganache.server();
+// Start a local Ethereum server
 const port = 8545;
-server.listen(port, (/*err, blockchain*/) => {
-  console.log(`Local Ethereum node running. Listening on port ${port}.`);
-});
+if (program.start) {
+  const server = ganache.server();
+  server.listen(port, (/*err, blockchain*/) => {
+    console.log(`Local Ethereum node running. Listening on port ${port}.`);
+  });
+}
