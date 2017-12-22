e86bc29c8726bed0658bc78496184265e4b685f3
Tal Ater <tal@talater.com>
2017-12-23 01:15:33 +0200

    Added help text to CLI

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const version = require('./lib/version');
 const OS = require('os');
 const ganache = require('ganache-cli');
 
+program.on('--help', () => {
+  console.log();
+  console.log('  Examples:');
+  console.log();
+  console.log('    - Start a local Ethereum node on the default port');
+  console.log('      $ dav-cli --start');
+  console.log();
+  console.log('    - Start a local Ethereum node on port 1234');
+  console.log('      $ dav-cli --start --port 1234');
+  console.log();
+  console.log('    - Start a local Ethereum node on port 1234');
+  console.log('      $ dav-cli --port 1234');
+  console.log();
+  console.log('  Find out more at https://developers.dav.network');
+});
+
 // Configure the CLI
 program
   .version(version)
