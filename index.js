77b2c14eed633ab312be4b177a22c8b894d1aff1
Tal Ater <tal@talater.com>
2017-12-23 01:16:08 +0200

    Only determine port if need to start local node

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ program
 console.log(`DAV CLI v${version} - makes developing with DAV easy`+OS.EOL);
 
 // Start a local Ethereum server
-const port = program.port || 8545;
 if (program.start || program.port) {
+  const port = program.port || 8545;
   const server = ganache.server();
   server.listen(port, (/*err, blockchain*/) => {
     console.log(`Local Ethereum node running. Listening on port ${port}.`);
