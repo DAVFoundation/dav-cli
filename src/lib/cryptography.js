88d9535370b016aa73faff284ae89a9c7fcf91b5
Tal Ater <tal@talater.com>
2018-01-13 13:31:54 +0200

    Added jsdoc to cryptography file

--- src/lib/cryptography.js
+++ src/lib/cryptography.js
@@ -1,6 +1,12 @@
 const keythereum = require('keythereum');
 const defaultPassword = '';
 
+/**
+ * Generates a new private key and returns it in keystore secret-storage format
+ *
+ * @param {string} password Password to lock keystore with
+ * @returns {object}
+ */
 const createPrivateKey = (password = defaultPassword) => {
   const privateKey = keythereum.create();
   return keythereum.dump(
